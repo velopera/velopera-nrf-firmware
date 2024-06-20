65a1d0ffd2499c380530bbb2a582222b685f0015
Simon VELOpera Vogl <simon+velopera@voxel.at>
2024-06-20 12:28:03 +0200

    added option to output compile_commands

--- compile.sh
+++ compile.sh
@@ -11,5 +11,7 @@ cd ${current_dir}
 TOP=~/proj/velopera/repos/velopera-nrf-firmware
 west build --build-dir ${TOP}/build ${TOP} \
 	--pristine --board nrf9160dk_nrf9160_ns \
-	--no-sysbuild -- -DNCS_TOOLCHAIN_VERSION=NONE -DCONF_FILE=${TOP}/prj.conf; \
-	${TOP}/boards/nrf9160dk_nrf9160_ns.conf -DDTC_OVERLAY_FILE=${TOP}/boards/nrf9160dk_nrf9160_ns.overlay
+	--no-sysbuild -- -DNCS_TOOLCHAIN_VERSION=NONE -DCONF_FILE=${TOP}/prj.conf \
+	${TOP}/boards/nrf9160dk_nrf9160_ns.conf -DDTC_OVERLAY_FILE=${TOP}/boards/nrf9160dk_nrf9160_ns.overlay \
+	-G'Unix Makefiles' -DCMAKE_VERBOSE_MAKEFILE=ON -DCCACHE_ENABLE=0 \
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=1
